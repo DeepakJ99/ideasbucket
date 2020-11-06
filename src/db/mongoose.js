b89f919a2210f5df0304e203f81cfc9db91f1ac5
Deepak Jakhu <djakhu99@gmail.com>
2020-11-06 19:13:14 +0530

    something

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,8 @@
-const { mongo } = require("mongoose")
 
 const mongoose = require('mongoose')
-var url = 'mongodb+srv://ideas-bucket:hU3ExB8sXHSRLn5@cluster0.ebtdd.mongodb.net/ideasbucket?retryWrites=true&w=majority'
-mongoose.connect(url,{
+var uri = 'mongodb+srv://ideas-bucket:hU3ExB8sXHSRLn5@cluster0.ebtdd.mongodb.net/ideasbucket?retryWrites=true&w=majority'
+//var url = 'mongodb+srv://ideas-bucket:hU3ExB8sXHSRLn5@cluster0.ebtdd.mongodb.net/ideasbucket?retryWrites=true&w=majority'
+mongoose.connect(uri,{
     useNewUrlParser : true,
     userCreateIndex : true
 })
