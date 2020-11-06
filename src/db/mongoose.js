fe820cd8841f4c067d74ac9f48e4bd3fc267a838
Deepak Jakhu <djakhu99@gmail.com>
2020-11-06 18:49:25 +0530

    mongodb atlas

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const { mongo } = require("mongoose")
 
 const mongoose = require('mongoose')
-var url = process.env.MONGOLAB_URI;
+var url = 'mongodb+srv://ideas-bucket:hU3ExB8sXHSRLn5@cluster0.ebtdd.mongodb.net/ideasbucket?retryWrites=true&w=majority'
 mongoose.connect(url,{
     useNewUrlParser : true,
     userCreateIndex : true
